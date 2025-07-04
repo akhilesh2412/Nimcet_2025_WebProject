a75c38ddb235013772a363f1429040c6a8f7c766
Akhilesh Verma <akhileshwebz@gmail.com>
2025-07-04 09:08:52 +0000

    remove this live theme add another stylish theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,6 @@ import { Toaster } from '@/components/ui/toaster';
 import { AuthProvider } from '@/context/AuthContext';
 import { AuthWrapper } from '@/components/AuthWrapper';
 import { ThemeProvider } from '@/components/theme-provider';
-import { BackgroundAnimation } from '@/components/background-animation';
 
 export const metadata: Metadata = {
   title: 'Course Compass',
@@ -31,11 +30,10 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <BackgroundAnimation />
           <AuthProvider>
               <AuthWrapper>
                   <Header />
-                  <main className="flex-grow container mx-auto px-4 sm:px-6 lg:px-8 py-8 relative z-10 bg-transparent">
+                  <main className="flex-grow container mx-auto px-4 sm:px-6 lg:px-8 py-8">
                     {children}
                   </main>
                   <Toaster />
